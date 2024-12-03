ed2efd2e2f55aa5557f83620843f802bbb11323e
AxsGro <ale.silva.gro@outlook.es>
2024-12-03 13:59:47 -0600

    a

--- src/Data/repositories/AuthRepository.tsx
+++ src/Data/repositories/AuthRepository.tsx
@@ -1,21 +1,21 @@
 import { AuthRepository } from "../../Domain/repositories/AuthRepository";
 import { User } from "../../Domain/entities/User";
-import { ResponseApiDelivery } from "../../Data/sources/remote/models/ResponseApiDelivery"; 
+import { ResponseAPIDelivery } from "../../Data/sources/remote/models/ResponseApiDelivery"; 
 import { ApiDelivery } from "../../Data/sources/remote/api/ApiDelivery";
 import { AxiosError } from "axios";
 export class AuthRepositoryImplement implements AuthRepository{
 
-    async register(user: User): Promise<ResponseApiDelivery>{
+    async register(user: User): Promise<ResponseAPIDelivery>{
 
         try{
 
-            const response = await ApiDelivery.post<ResponseApiDelivery>('/users/create', user);
+            const response = await ApiDelivery.post<ResponseAPIDelivery>('/users/create', user);
             return Promise.resolve(response.data);
 
         }catch (error){
             let e = (error as AxiosError);
             console.log('Error: ' + JSON.stringify(e.response?.data));
-            const apiError:ResponseApiDelivery = JSON.parse(JSON.stringify(e.response?.data);
+            const apiError:ResponseAPIDelivery = JSON.parse(JSON.stringify(e.response?.data);
             return Promise.resolve(apiError);
         }
     }
